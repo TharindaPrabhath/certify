f6f4d259b20706fb279c5ff0077a9772eedcf9f0
tharinda <tharindahp@gmail.com>
2021-08-18 19:36:30 +0530

    created

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../components/Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Sidebar</h2>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../components/Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
