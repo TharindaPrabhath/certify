a5c32715ebd4d6bfa86d7806ac5fe6da5a5766d4
tharinda <tharindahp@gmail.com>
2021-08-18 19:36:35 +0530

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="left-col">
+        <Sidebar />
+      </div>
+
+      <div className="right-col">
+        <h2>Screens</h2>
+      </div>
     </div>
   );
 }
