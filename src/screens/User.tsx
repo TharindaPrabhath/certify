3e1148c0c2d09da64da6ad9e621347c0147e904a
tharinda <tharindahp@gmail.com>
2021-08-19 23:31:48 +0530

    created

--- src/screens/User.tsx
+++ src/screens/User.tsx
@@ -2,10 +2,32 @@ import React from "react";
 
 import "../screens/User.css";
 
+import { Button } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import UserTable from "../components/UserTable";
+
 const User = () => {
   return (
     <div className="user">
-      <h2>User</h2>
+      <div className="user__content">
+        <div className="top">
+          <h2>Users</h2>
+          <Button
+            style={{
+              backgroundColor: "#0eadec",
+              fontWeight: 700,
+              textTransform: "capitalize",
+            }}
+          >
+            <AddIcon />
+            New User
+          </Button>
+        </div>
+
+        <div className="table">
+          <UserTable />
+        </div>
+      </div>
     </div>
   );
 };
