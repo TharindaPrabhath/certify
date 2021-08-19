4a4a101b2f3c04b92bc45e46379b9c14bbc92c99
tharinda <tharindahp@gmail.com>
2021-08-19 10:06:29 +0530

    created navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import "./App.css";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Dashboard from "./screens/Dashboard";
+import Topbar from "./components/Topbar";
+import User from "./screens/User";
 
 function App() {
   return (
-    <div className="App">
-      <div className="left-col">
-        <Sidebar />
-      </div>
+    <Router>
+      <div className="app">
+        <div className="app__topbar">
+          <Topbar />
+        </div>
 
-      <div className="right-col">
-        <h2>Screens</h2>
+        <div className="app__sidebar">
+          <Sidebar />
+        </div>
+
+        <div className="app__screen">
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/user">
+            <User />
+          </Route>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
