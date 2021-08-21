0682db6fc3b99679d18a56c0c400775102eb3191
tharinda <tharindahp@gmail.com>
2021-08-21 14:16:13 +0530

    added breadcrumbs

--- src/screens/User.tsx
+++ src/screens/User.tsx
@@ -5,23 +5,23 @@ import "../screens/User.css";
 import { Button } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import UserTable from "../components/UserTable";
+import { Link } from "react-router-dom";
+import { useButtonStyles } from "../data/styles";
 
 const User = () => {
+  const buttonStyles = useButtonStyles();
+
   return (
     <div className="user">
       <div className="user__content">
         <div className="top">
           <h2>Users</h2>
-          <Button
-            style={{
-              backgroundColor: "#0eadec",
-              fontWeight: 700,
-              textTransform: "capitalize",
-            }}
-          >
-            <AddIcon />
-            New User
-          </Button>
+          <Link to="user/new">
+            <Button className={buttonStyles.standardBtn}>
+              <AddIcon />
+              New User
+            </Button>
+          </Link>
         </div>
 
         <div className="table">
