ab541f681fe0bf31c52cddb30fdf0cf82370ad35
tharinda <tharindahp@gmail.com>
2021-08-22 19:29:55 +0530

    added breadcrumb

--- src/screens/User.tsx
+++ src/screens/User.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import "../screens/User.css";
 
-import { Button } from "@material-ui/core";
+import { Breadcrumbs, Button, Typography } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import UserTable from "../components/UserTable";
 import { Link } from "react-router-dom";
 import { useButtonStyles } from "../data/styles";
+import colors from "../data/colors";
 
 const User = () => {
   const buttonStyles = useButtonStyles();
@@ -15,13 +16,22 @@ const User = () => {
     <div className="user">
       <div className="user__content">
         <div className="top">
-          <h2>Users</h2>
-          <Link to="user/new">
-            <Button className={buttonStyles.standardBtn}>
-              <AddIcon />
-              New User
-            </Button>
-          </Link>
+          <div className="left-col">
+            <h2>Users</h2>
+            <Breadcrumbs aria-label="breadcrumb" style={{ color: "white" }}>
+              <Link to="/dashboard">Dashboard</Link>
+              <Typography style={{ color: colors.dimmedClr }}>Users</Typography>
+            </Breadcrumbs>
+          </div>
+
+          <div className="right-col">
+            <Link to="user/new">
+              <Button className={buttonStyles.standardBtn}>
+                <AddIcon />
+                New User
+              </Button>
+            </Link>
+          </div>
         </div>
 
         <div className="table">
