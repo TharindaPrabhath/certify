51c2918f1353afb8ef6ae5c8e2f3c7cb54daab81
tharinda <tharindahp@gmail.com>
2021-08-23 21:58:32 +0530

    created

---
+++ src/components/Certificate.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import "./Certificate.css";
+
+import logo from "../assets/logo/logo.png";
+
+const Certificate = () => {
+  return (
+    <div className="certificate">
+      <div className="certificate__content">
+        <div className="header">
+          <div className="logo">
+            <img src={logo} alt="Logo" width={32} />
+            <h2>Certify</h2>
+          </div>
+        </div>
+
+        <div className="body">
+          <div className="reciever-details">
+            <h1 className="reciever">Tharinda P Anurajeewa</h1>
+            <p className="description">
+              is hereby awarded this certificate of achievement for the
+              contribution of <span>Supplying educational materials</span> on
+              2021 Aug 23
+            </p>
+          </div>
+
+          <div className="issuer-details">
+            <div className="left-col">
+              <h4 className="sign-person-1">President-Certify Inc.</h4>
+            </div>
+
+            <div className="middle-col"></div>
+
+            <div className="right-col">
+              <h4 className="sign-person-2">Vice President-Certify Inc.</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer">
+          <h5 className="certificate-id">
+            Certificate ID:<span> 123-567-890</span>
+          </h5>
+          <h5 className="verification-web-site">
+            Verify the authenticity of the certificate at:
+            <span> www.certify/certificate/verify.com</span>
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Certificate;
--- src/components/CertificateTable.tsx
+++ src/components/CertificateTable.tsx
@@ -1,8 +1,242 @@
-import { Paper } from "@material-ui/core";
+import {
+  Button,
+  Checkbox,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@material-ui/core";
 import React from "react";
+import { useButtonStyles, useTableStyles } from "../data/styles";
+import { CertificateTableProp, TableHeadCellProp } from "../types/TableProp";
+import TableToolbar from "./TableToolbar";
+
+const headCells: TableHeadCellProp[] = [
+  {
+    id: "certificateId",
+    name: "CertificateId",
+    numeric: false,
+    label: "CertificateId",
+  },
+  { id: "recievedBy", name: "RecievedBy", numeric: false, label: "RecievedBy" },
+  { id: "issuedBy", name: "IssuedBy", numeric: false, label: "IssuedBy" },
+  { id: "type", name: "Type", numeric: false, label: "Type" },
+  { id: "issuedDate", name: "IssuedDate", numeric: false, label: "IssuedDate" },
+];
+
+const getRow = (
+  certificateId: string,
+  recievedBy: string,
+  issuedBy: string,
+  type: string,
+  issuedDate: string
+): CertificateTableProp => {
+  return { certificateId, recievedBy, issuedBy, type, issuedDate };
+};
+
+const rows: CertificateTableProp[] = [
+  getRow("001", "Tharinda P", "Lasana", "Participation", "2001.03.12"),
+  getRow("002", "Tharinda P", "Lasana", "Participation", "2001.03.12"),
+  getRow("003", "Tharinda P", "Lasana", "Participation", "2001.03.12"),
+  getRow("004", "Tharinda P", "Lasana", "Participation", "2001.03.12"),
+  getRow("005", "Tharinda P", "Lasana", "Participation", "2001.03.12"),
+  getRow("006", "Tharinda P", "Lasana", "Participation", "2001.03.12"),
+  getRow("007", "Tharinda P", "Lasana", "Participation", "2001.03.12"),
+];
+
+const getSuggestions = (): string[] => {
+  var arr: string[] = [];
+
+  rows.map((row) => arr.push(row.recievedBy));
+  return arr;
+};
 
 const CertificateTable = () => {
-  return <Paper></Paper>;
+  const [selected, setSelected] = React.useState<string[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const tableStyles = useTableStyles();
+  const buttonStyles = useButtonStyles();
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.certificateId);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected: string[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+
+  return (
+    <Paper className={tableStyles.paper} style={{ borderRadius: "1em" }}>
+      <TableToolbar
+        numSelected={selected.length}
+        suggestions={getSuggestions()}
+      />
+      <TableContainer>
+        <Table
+          size="medium"
+          className={tableStyles.table}
+          aria-label="enhanced table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell padding="checkbox">
+                <Checkbox
+                  color="primary"
+                  inputProps={{ "aria-label": "decorative checkbox" }}
+                  onChange={(event) => handleSelectAllClick(event)}
+                />
+              </TableCell>
+              {headCells.map((headCell, index) => {
+                return (
+                  <TableCell
+                    key={index}
+                    align="left"
+                    padding="normal"
+                    className={tableStyles.tableHeaderCell}
+                  >
+                    {headCell.label}
+                  </TableCell>
+                );
+              })}
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                const isItemSelected = isSelected(row.certificateId);
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    className={tableStyles.tableRow}
+                    key={index}
+                    hover={true}
+                    role="checkbox"
+                    onClick={(event) => handleClick(event, row.certificateId)}
+                    aria-checked={isItemSelected}
+                    selected={isItemSelected}
+                    tabIndex={-1}
+                  >
+                    <TableCell
+                      padding="checkbox"
+                      className={tableStyles.tableCell}
+                    >
+                      <Checkbox
+                        checked={isItemSelected}
+                        inputProps={{ "aria-labelledby": labelId }}
+                        color="primary"
+                      />
+                    </TableCell>
+
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      padding="normal"
+                      className={tableStyles.tableCell}
+                    >
+                      {row.certificateId}
+                    </TableCell>
+                    <TableCell
+                      align="left"
+                      className={tableStyles.tableCell}
+                      style={{ fontWeight: 700 }}
+                    >
+                      {row.recievedBy}
+                    </TableCell>
+                    <TableCell align="left" className={tableStyles.tableCell}>
+                      {row.issuedBy}
+                    </TableCell>
+                    <TableCell align="left" className={tableStyles.tableCell}>
+                      {row.type}
+                    </TableCell>
+                    <TableCell align="left" className={tableStyles.tableCell}>
+                      {row.issuedDate}
+                    </TableCell>
+                    <TableCell className={tableStyles.tableCell}>
+                      <Button
+                        className={buttonStyles.editBtn}
+                        onClick={(e) => {
+                          // disable selection of the whole row on click
+                          e.preventDefault();
+                          e.stopPropagation();
+                        }}
+                      >
+                        Edit
+                      </Button>
+                    </TableCell>
+                    <TableCell className={tableStyles.tableCell}>
+                      <Button
+                        className={buttonStyles.deleteBtn}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          e.stopPropagation();
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        className={tableStyles.pagination}
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
 };
 
 export default CertificateTable;
