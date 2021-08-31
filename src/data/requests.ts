dc336189107999dce2595928ddfd5512d38d9650
tharinda <tharindahp@gmail.com>
2021-08-31 20:34:09 +0530

    created

---
+++ src/data/requests.ts
@@ -0,0 +1,10 @@
+const fetchAdmins = "/admin"
+const fetchUsers = "/user"
+const fetchCertificates = "/certificate"
+
+const postNewUser = "/user/add"
+const postNewCertificate = "/certificate/add"
+
+const requests = {fetchAdmins, fetchUsers, fetchCertificates, postNewUser, postNewCertificate}
+    
+export default requests;
