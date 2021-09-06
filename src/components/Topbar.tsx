e3bd694ca9d51f643114d55a3634796a2e96be0d
tharinda <tharindahp@gmail.com>
2021-09-06 10:11:22 +0530

    added toggle menu to avatar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,15 +3,56 @@ import React from "react";
 import "../components/Topbar.css";
 
 import MenuIcon from "@material-ui/icons/Menu";
-import { Avatar, Button, SwipeableDrawer } from "@material-ui/core";
+import {
+  Avatar,
+  Button,
+  ClickAwayListener,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+  SwipeableDrawer,
+} from "@material-ui/core";
 import { useState } from "react";
 import Sidebar from "./Sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../redux";
+import { ReducerType } from "../redux/store";
+import colors from "../data/colors";
 
 const Topbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const [avatarMenuOpen, setAvatarMenuOpen] = useState<boolean>(false);
+  const avatarBtnRef = React.useRef<HTMLButtonElement>(null);
+
+  const dispatch = useDispatch();
+  const { initAdmin, removeAdmin } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  const currentAdmin = useSelector(
+    (state: ReducerType) => state.adminReducer.currentAdmin
+  );
 
   const toggleSidebar = (open: boolean) => {
     setSidebarOpen(open);
+  };
+
+  const handleAvatarToggle = () => {
+    setAvatarMenuOpen(!avatarMenuOpen);
+  };
+
+  const handleClose = (event: React.MouseEvent<EventTarget>) => {
+    if (
+      avatarBtnRef.current &&
+      avatarBtnRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setAvatarMenuOpen(false);
   };
 
   return (
@@ -27,9 +68,49 @@ const Topbar = () => {
         >
           <Sidebar />
         </SwipeableDrawer>
-        <Avatar className="avatar" style={{ cursor: "pointer" }}>
-          TP
-        </Avatar>
+        <Button
+          className="avatar"
+          ref={avatarBtnRef}
+          aria-controls={avatarMenuOpen ? "menu-list-grow" : undefined}
+          aria-haspopup="true"
+          onClick={handleAvatarToggle}
+        >
+          <Avatar style={{ cursor: "pointer" }}>
+            {localStorage.getItem("currentAdmin")?.substring(0, 2)}
+          </Avatar>
+        </Button>
+
+        <Popper
+          open={avatarMenuOpen}
+          anchorEl={avatarBtnRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper
+                style={{
+                  backgroundColor: colors.secondaryBgClr,
+                  color: colors.secondaryFontClr,
+                }}
+              >
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={avatarMenuOpen} id="menu-list-grow">
+                    <MenuItem>Profile</MenuItem>
+                    <MenuItem>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
       </div>
     </div>
   );
