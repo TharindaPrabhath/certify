a07792d500ce0964d42305261ab75f0475f291b8
tharinda <tharindahp@gmail.com>
2021-09-06 10:14:36 +0530

    use local storage to get current admin

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -6,14 +6,31 @@ import "../screens/Dashboard.css";
 import CertificateImg from "../assets/certificate.png";
 import WelcomeImg from "../assets/welcome.svg";
 
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../redux";
+import { State } from "../redux/reducers/adminReducer";
+import { ReducerType } from "../redux/store";
+
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { initAdmin, removeAdmin } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  const currentAdmin = useSelector(
+    (state: ReducerType) => state.adminReducer.currentAdmin
+  );
+
   return (
     <div className="dashboard">
       <div className="dashboard__content">
         <div className="welcome-banner">
           <div className="welcome-banner__content">
             <div className="left-col">
-              <h1 className="greet">Hello Tharinda!</h1>
+              <h1 className="greet">
+                {"Hello " + localStorage.getItem("currentAdmin")}
+              </h1>
               <p className="des">Welcome to the Certify platform</p>
             </div>
 
