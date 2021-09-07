6b802e80fca658e5ca327237c0413644ba552fcf
tharinda <tharindahp@gmail.com>
2021-09-07 15:38:42 +0530

    -

--- src/components/core/CertifyTextField.tsx
+++ src/components/core/CertifyTextField.tsx
@@ -1,6 +1,6 @@
 import { TextField } from "@material-ui/core";
 import { FieldProps, getIn } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import { useTextfieldStyles } from "../../data/styles";
 
 const CertifyTextField: React.FC<
@@ -10,11 +10,14 @@ const CertifyTextField: React.FC<
     getIn(form.touched, field.name) && getIn(form.errors, field.name);
   const styles = useTextfieldStyles();
 
+  const [text, setText] = useState<any>();
+
   return (
     <TextField
       variant="outlined"
       color="primary"
       required={required}
+      //onChange={(e)=> setText(e.target.value)}
       error={!!error}
       helperText={error}
       multiline={textArea}
