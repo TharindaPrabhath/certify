507f21d3c8cb29b795a4e284eecc7ea4793c0b22
tharinda <tharindahp@gmail.com>
2021-09-07 15:40:00 +0530

    defined user and certificate action types

--- src/redux/actions/ActionTypes.ts
+++ src/redux/actions/ActionTypes.ts
@@ -2,3 +2,13 @@ export enum AdminActionType{
     INITIATE_ADMIN,
     REMOVE_ADMIN
 }
+
+export enum UserActionType{
+    SET_USER,
+    REMOVE_USER
+}
+
+export enum CertificateActionType{
+    SET_CERTIFICATE,
+    REMOVE_CERTIFICATE
+}
