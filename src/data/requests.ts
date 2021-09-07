c9289af7fa1a2ee8e44958717e7b9131ae814d45
tharinda <tharindahp@gmail.com>
2021-09-07 15:38:54 +0530

    added more requests

--- src/data/requests.ts
+++ src/data/requests.ts
@@ -1,12 +1,21 @@
-const login = "/auth"
+const login = "/auth/login"
 
 const fetchAdmins = "/admin"
+const fetchUser = "/user/get"
 const fetchUsers = "/user"
 const fetchCertificates = "/certificate"
 
 const postNewUser = "/user/add"
 const postNewCertificate = "/certificate/add"
 
-const requests = {login, fetchAdmins, fetchUsers, fetchCertificates, postNewUser, postNewCertificate}
+const updateUser = "/user/update"
+const updateCertificate = "/certificate/update"
+
+const deleteUser = "/user/delete"
+const deleteCertificate = "/certificate/delete"
+
+const requests = {login, fetchAdmins, fetchUser, fetchUsers, 
+                fetchCertificates, postNewUser, postNewCertificate, 
+                updateUser, updateCertificate, deleteUser, deleteCertificate}
     
 export default requests;
