4b50e92028034c40e336904415bc0dcb7cf41b2e
tharinda <tharindahp@gmail.com>
2021-09-07 15:39:14 +0530

    added user and certificate reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,9 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 import adminReducer from "./reducers/adminReducer"
+import userReducer from "./reducers/userReducer"
+import certificateReducer from "./reducers/certificateReducer"
 
 const reducer = combineReducers({
-    adminReducer
+    adminReducer,
+    userReducer,
+    certificateReducer
 })
 
 const store = createStore(
