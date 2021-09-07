519bf87f3ecc7b797e80b94dd970d71370e5f29f
tharinda <tharindahp@gmail.com>
2021-09-07 15:43:17 +0530

    exported api base url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api/v1";
+export const API_BASE_URL = "http://localhost:8080/api/v1";
 export const TOKEN = localStorage.getItem("token");
 
 const axiosInstance = axios.create(
