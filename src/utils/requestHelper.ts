a70a1eebf5fa06fa8d74f8065ddcc49565652cea
tharinda <tharindahp@gmail.com>
2021-09-11 18:47:10 +0530

    defined more requests functions

--- src/utils/requestHelper.ts
+++ src/utils/requestHelper.ts
@@ -3,6 +3,15 @@ import CertificateDto from "../types/models/CertificateDto";
 import UserDto from "../types/models/UserDto";
 import axiosInstance from "./axios";
 
+export function validateToken(){
+    return axiosInstance.get(requests.validateToken);
+}
+
+export async function fetchAdminByUsername(username: string) {
+    return await axiosInstance.get(`${requests.fetchAdminByUsername}/${username}`);
+}
+
+
 export async function fetchUsers(){
     return await axiosInstance.get(requests.fetchUsers);
 }
@@ -19,8 +28,14 @@ export async function deleteUser(uid: number){
     return await axiosInstance.delete(`${requests.deleteUser}/${uid}`)
 }
 
+
+
 export async function fetchCertificates(){
     return await axiosInstance.get(requests.fetchCertificates);
+}
+
+export async function addCertificate(certificate: any){
+    return await axiosInstance.post(requests.postNewCertificate, certificate);
 }
 
 export async function updateCertificate(id: number, updatedCertificate: CertificateDto){
@@ -29,4 +44,13 @@ export async function updateCertificate(id: number, updatedCertificate: Certific
 
 export async function deleteCertificate(id: number){
     return await axiosInstance.delete(`${requests.deleteCertificate}/${id}`)
+}
+
+
+export async function fetchUsersAnalytics(){
+    return await axiosInstance.get(requests.fetchUsersAnalytics);
+}
+
+export async function fetchCertificatesAnalytics(){
+    return await axiosInstance.get(requests.fetchCertificatesAnalytics);
 }
