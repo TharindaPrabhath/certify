1f12375e76708c643e664db9134c4bffa9d57724
tharinda <tharindahp@gmail.com>
2021-09-17 10:53:59 +0530

    created

---
+++ src/utils/useAuth.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import requests from "../data/requests";
+import { API_BASE_URL } from "./axios";
+import useTokenService from "./useTokenService";
+
+const useAuth = () => {
+    const {getAccessToken} = useTokenService();
+
+    const signin = async (values: any) => {
+        const res = await axios.post(API_BASE_URL + requests.login, values);
+        return res;
+    }
+
+    const logout = async () => {
+
+    }
+
+    const isAdminLoggedIn = (): boolean => {
+        const accessToken = getAccessToken();
+        if(accessToken === null || accessToken === undefined) return false;
+        return true;
+    } 
+
+    return {signin, logout, isAdminLoggedIn};
+
+}
+
+export default useAuth;
