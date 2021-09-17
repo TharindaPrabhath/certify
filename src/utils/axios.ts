8955779f97cab58300b9b9eb8633d5238c810afb
tharinda <tharindahp@gmail.com>
2021-09-17 10:53:37 +0530

    deprecated axios interceptors response config

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,19 +1,36 @@
 import axios from "axios";
+import useTokenService from "./useTokenService";
 
 export const API_BASE_URL = "http://localhost:8080/api/v1";
-export const TOKEN = localStorage.getItem("token");
 
-const axiosInstance = axios.create(
-    {
-        headers: {
-            Authorization: TOKEN, 
-        },
+const useAxios = () => {
+    const { getAccessToken } = useTokenService();
+    const axiosInstance = axios.create(
+        {
+            headers: {
+                Authorization: `Bearer ${getAccessToken()}`, 
+            },
+    
+            baseURL: API_BASE_URL
+            
+        }
+    );
 
-        baseURL: API_BASE_URL
-        
-    }
-);
+    // axiosInstance.interceptors.response.use(
+    //     (res)=>
+    //     new Promise((resolve, reject)=>{
+    //         resolve(res);
+    //     }),
+    //     (err)=>{
+    //         if(err.response.status === 403){
+    //             window.location.pathname = "/signin";
+    //         }
+            
+    //     }
+    // )
+    return axiosInstance;
+    
+}
 
 
-
-export default axiosInstance;
+export default useAxios;
