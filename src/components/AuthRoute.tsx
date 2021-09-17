19d04f474b2f77cee33352b1d891b6844a3c2023
tharinda <tharindahp@gmail.com>
2021-09-17 10:37:41 +0530

    added useAuth

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -1,10 +1,12 @@
 import { Redirect, Route, RouteProps } from "react-router";
-import { isAdminLoggedIn } from "../utils/auth";
+import useAuth from "../utils/useAuth";
 
 // when try to navigate to the signin page after a successful login,
 // below code block helps to redirect back to the dashboard page :)
 
 const AuthRoute: React.FC<RouteProps> = ({ ...rest }) => {
+  const { isAdminLoggedIn } = useAuth();
+
   if (isAdminLoggedIn() && rest.path === "/signin")
     return <Redirect to="/dashboard" />;
   return <Route {...rest} />;
