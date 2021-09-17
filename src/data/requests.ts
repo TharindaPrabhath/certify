c87fe6612b59a0722b6772ed9912cdd6dadbc146
tharinda <tharindahp@gmail.com>
2021-09-17 10:42:21 +0530

    defined fetchCertificate, validateCertificate endpoints

--- src/data/requests.ts
+++ src/data/requests.ts
@@ -1,10 +1,12 @@
 const login = "/auth/login"
 const validateToken = "/auth/validateToken"
+const validateCertificate = "/certificate/validate"
 
 const fetchAdminByUsername = "/admin/get/byUsername"
 const fetchAdmins = "/admin"
 const fetchUser = "/user/get"
 const fetchUsers = "/user"
+const fetchCertificate = "/certificate/get"
 const fetchCertificates = "/certificate"
 const fetchUsersAnalytics = "/analytics/user"
 const fetchCertificatesAnalytics = "/analytics/certificate"
@@ -18,7 +20,7 @@ const updateCertificate = "/certificate/update"
 const deleteUser = "/user/delete"
 const deleteCertificate = "/certificate/delete"
 
-const requests = {login, validateToken, fetchAdminByUsername , fetchAdmins, fetchUser, fetchUsers, 
+const requests = {login, validateToken, validateCertificate, fetchAdminByUsername , fetchAdmins, fetchUser, fetchUsers, fetchCertificate,
                 fetchCertificates, fetchUsersAnalytics, fetchCertificatesAnalytics, postNewUser, postNewCertificate, 
                 updateUser, updateCertificate, deleteUser, deleteCertificate}
     
