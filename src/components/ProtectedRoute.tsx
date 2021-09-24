db293fa4adf7fb19285f6749c5a2d61d140fd00c
tharinda <tharindahp@gmail.com>
2021-09-24 20:34:10 +0530

    -

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Redirect, Route, RouteProps } from "react-router";
 import useAuth from "../utils/useAuth";
 
@@ -8,12 +7,11 @@ interface ProtectedRouteProps extends RouteProps {}
 // below code block helps to redirect back to the signin page :)
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ ...rest }) => {
-  const [accessToken, setAccessToken] = useState<boolean>(false);
   const { isAdminLoggedIn } = useAuth();
 
-  // if (accessToken && rest.path !== "/signin") return <Route {...rest} />;
-  // return <Redirect to="/signin" />;
-  return <Route {...rest} />;
+  if (isAdminLoggedIn() && rest.path !== "/signin") return <Route {...rest} />;
+  return <Redirect to="/signin" />;
+  //return <Route {...rest} />;
 };
 
 export default ProtectedRoute;
