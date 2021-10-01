83550633ada71ab4e4c985180f94f3c0f4c8c40f
tharinda <tharindahp@gmail.com>
2021-10-01 17:14:51 +0530

    added refreshing the expired acces token functionality

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
+import requests from "../data/requests";
+import useLocalStorage from "./useLocalStorage";
 import useTokenService from "./useTokenService";
 
-// export const API_BASE_URL = "http://localhost:8080/api/v1";
+//export const API_BASE_URL = "http://localhost:8080/api/v1";
 export const API_BASE_URL = "https://symetry-certify.herokuapp.com/api/v1"
 const useAxios = () => {
-    const { getAccessToken } = useTokenService();
+    const { setAccessToken, setAccessTokenExpiresAt, 
+        getAccessToken, getRefreshToken, 
+        getAccessTokenExpiresAt } = useTokenService();
+    const { getAdmin } = useLocalStorage();
     const axiosInstance = axios.create(
         {
             headers: {
@@ -16,18 +21,27 @@ const useAxios = () => {
         }
     );
 
-    // axiosInstance.interceptors.response.use(
-    //     (res)=>
-    //     new Promise((resolve, reject)=>{
-    //         resolve(res);
-    //     }),
-    //     (err)=>{
-    //         if(err.response.status === 403){
-    //             window.location.pathname = "/signin";
-    //         }
-            
-    //     }
-    // )
+    axiosInstance.interceptors.request.use(async (config) => {
+      const expireAt = getAccessTokenExpiresAt();
+      if (Date.now() > Date.parse(expireAt!)) {
+        // access token has expired
+        // refreshing the access token
+        await axios.post(API_BASE_URL+requests.refreshToken, {
+          refreshToken: getRefreshToken(),
+          accessToken: getAccessToken(),
+          adminId: getAdmin().id,
+        }).then((res) => {
+          // setting updated token
+          setAccessToken(res.data.accessToken)
+          setAccessTokenExpiresAt(res.data.accessTokenExpiresAt)
+        }).catch((err)=> console.error(err))
+       
+      }
+      return config;
+    }, (err) => {
+       console.error("error in getting ",err)
+    });
+
     return axiosInstance;
     
 }
