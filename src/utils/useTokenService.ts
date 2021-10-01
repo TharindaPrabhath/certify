4bf8ccdde6621855295037b2dd2a56fd0584c8b2
tharinda <tharindahp@gmail.com>
2021-10-01 17:15:13 +0530

    -

--- src/utils/useTokenService.ts
+++ src/utils/useTokenService.ts
@@ -3,16 +3,54 @@ const useTokenService = () => {
     const setAccessToken = (accessToken: string) => {
         localStorage.setItem("accessToken", accessToken);
     }
+
+    const setRefreshToken = (refreshToken: string) => {
+        localStorage.setItem("refreshToken", refreshToken);
+    }
+
+    const setAccessTokenExpiresAt = (expiresAt: string) => {
+        localStorage.setItem("accessTokenExpiresAt", expiresAt);
+    }
+
+    const setRefreshTokenExpiresAt = (expiresAt: string) => {
+        localStorage.setItem("refreshTokenExpiresAt", expiresAt);
+    }
     
     const getAccessToken = () => {
         return localStorage.getItem("accessToken");
+    }
+
+    const getRefreshToken = () => {
+        return localStorage.getItem("refreshToken");
+    }
+
+    const getAccessTokenExpiresAt = () => {
+        return localStorage.getItem("accessTokenExpiresAt");
+    }
+
+    const getRefreshTokenExpiresAt = () => {
+        return localStorage.getItem("refreshTokenExpiresAt");
     }
     
     const removeAccessToken = () => {
         localStorage.removeItem("accessToken");
     }
 
-    return {setAccessToken, getAccessToken, removeAccessToken}
+    const removeRefreshToken = () => {
+        localStorage.removeItem("refreshToken");
+    }
+
+    const removeAccessTokenExpiresAt = () => {
+        localStorage.removeItem("accessTokenExpiresAt");
+    }
+
+    const removeRefreshTokenExpiresAt = () => {
+        localStorage.removeItem("refreshTokenExpiresAt");
+    }
+
+    return {setAccessToken, setRefreshToken, setAccessTokenExpiresAt, setRefreshTokenExpiresAt, getAccessToken, 
+        getRefreshToken, getAccessTokenExpiresAt, getRefreshTokenExpiresAt, removeAccessToken, removeRefreshToken,
+        removeAccessTokenExpiresAt, removeRefreshTokenExpiresAt}
 }
 
 export default useTokenService;
