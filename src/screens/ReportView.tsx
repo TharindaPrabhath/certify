97071541e31d94500d02a7127a0d45778778b65d
tharinda <tharindahp@gmail.com>
2021-11-01 19:40:46 +0530

    created

---
+++ src/screens/ReportView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ReportView.css";
+
+const ReportView = () => {
+  return (
+    <div className="report-view-screen">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default ReportView;
